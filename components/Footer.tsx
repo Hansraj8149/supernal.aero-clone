504c8c40fa6d8721c6c0c5ef0be736704689b5a8
Hansraj8149 <hansrajsaini8149@gmail.com>
2024-04-23 17:19:45 +0530

    made the footer and header responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,23 @@
 import React, { ReactElement } from 'react'
 import { navLinks, secondaryLinks,socials } from '@/utils'
+
+
 const Footer = () => {
   return (
     <footer className='h-full overflow-hidden bg-black'>
-        <div className='aw-container mt-20 mb-10'>
-            <div className='px-[80px] grid grid-cols-12 gap-[24px]'>
-                <div className="col-span-4">
+    <div className='aw-container mt-28 mb-10'>
+            <div className='flex flex-col items-center justify-center md:grid md:grid-cols-10 lg:grid lg:grid-cols-10 gap-[60px] md:gap-[24px]  lg:gap-[24px]'>
+                <div className="col-span-10 md:col-span-5 lg:col-span-5 items-center">
 
-                <img src='images/logo.svg' className='w-[100%]' alt='logo image'/>
+                <img src='images/logo.svg' className=' w-[100%] md:w-[70%] lg:w-[70%]' alt='logo image'/>
 
                 </div>
+                <div className='col-span-5 grid grid-cols-2 gap-20 items-start'>
 
-                <ul className="col-start-7 col-span-3 mx-[24px] p-0 list-none">
-                    {navLinks.map((footerLink) => (
+
+                <ul className=" col-span-1 mx-[24px] p-0 list-none">
+                {navLinks.map((footerLink) => (
+
                         <li key={footerLink.id} className='w-[100%] '>
 
                         <a  href={footerLink.href} className='flex items-center text-white text-[24px] h-[36px] leading-none transition duration-300 ease-in-out mb-[16px] hover:text-[#60bfff] hover:no-underline'>{footerLink.label}</a>
@@ -20,24 +25,27 @@ const Footer = () => {
 
                     ))}
                 </ul>
-                <div className="col-start-10 col-span-3 ">
-                    <div className="flex flex-col gap-[24px]">
+                <div className=" col-span-1 ">
+                <div className="flex flex-col gap-[24px]">
                         {secondaryLinks.map((secondaryLinks)=> (
                             <a key={secondaryLinks.id} href={secondaryLinks.href} className='flex text-[14px] leading-4 text-white hover:underline '>{secondaryLinks.label}</a>
                         ))}
                       
                     </div>
                     <div className="flex items-center gap-[24px] mt-[24px]">
-                        {socials.map((socials)=> (
+                    {socials.map((socials)=> (
+
                             <a key={socials.id} href={socials.href} className='text-white transition duration-200 text-[32px] hover:text-[#60bfff] no-underline'>{socials.component}</a>
                         ))}
                     </div>
                 </div>
             </div>
+            </div>
+
 
       </div>
             <div className="h-[200px] w-[100%] overflow-hidden bg-cover bg-top " style={{backgroundImage:`url("/images/footerBg.svg")`}}></div>
-    </footer>
+            </footer>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ const Header = () => {
     <header className='h-screen w-[100%] overflow-hidden bg-white'>
       <div className='aw-container mt-10 mb-10'>
         <div className="w-[100%] flex items-center justify-center mb-5">
-        <img src="/images/logo.png" className="w-[400px] h-[100px] border border-black" alt='blue point logo' />
+        <img src="/images/logo.png" className=" w-[200px] h-[50px] lg:w-[400px] lg:h-[100px] border border-black" alt='blue point logo' />
         </div>
-          <div className="w-[100%] h-[100vh-112px] flex items-center pt-[48px] gap-[24px]  ">
-            <ul className="w-[100%] list-none pl-[16px] mr-[32px] border-black border-l-[2px] relative">
+          <div className="w-[100%] h-[100vh-112px] flex flex-col md:flex-row lg:flex-row items-center pt-[48px] lg:gap-[24px]  ">
+            <ul className="w-[100%] list-none h-full pl-[16px] mr-[32px] border-black border-l-[2px] relative">
               {headerLinks.map((headerLink) => (
-                <li key={headerLink.id} className="h-[92px] flex items-center aw-nav-item ">
-                  <a href={headerLink.href} className="text-[32px] lg:text-[64px] aw-nav-link">{headerLink.label}</a>
+                <li key={headerLink.id} className="h-[70px] lg:h-[92px] flex items-center aw-nav-item ">
+                  <a href={headerLink.href} className="text-[40px] lg:text-[64px] aw-nav-link">{headerLink.label}</a>
                   <div className="aw-nav-item-image">
                     <img src={headerLink.imgSrc} alt={headerLink.label} className='w-[100%] h-[100%] object-cover object-center' />
                   </div>
@@ -22,25 +22,29 @@ const Header = () => {
               ))}
             </ul>
 
-            <div className="w-[100%] max-w-[390px] font-normal flex flex-col items-end justify-end">
+            <div className="w-[100%] max-w-[390px] font-normal items-center justify-between flex md:flex-col lg:flex-col  md:items-end md:justify-end lg:items-end lg:justify-end">
               <ul className="mt-[24px] p-0 list-none">
                 {navLinks.map((navLink) => (
-                  <li key={navLink.id} className="w-[100%] flex flex-col items-end justify-end hover:underline">
-                    <a href={navLink.href} className="flex items-center text-black text-[24px] lg:text-[32px] h-[36px] transition duration-200 ease-in-out mb-[16px]">{navLink.label}</a>
+                  <li key={navLink.id} className="w-[100%] flex flex-col items-start justify-start md:items-end md:justify-end  lg:items-end lg:justify-end hover:underline">
+                    <a href={navLink.href} className="flex items-center text-black text-[20px] lg:text-[32px] h-[36px] transition duration-200 ease-in-out mb-[5px] md:mb-[10px] lg:mb-[16px]">{navLink.label}</a>
                   </li>
                 ))}
               </ul>
               <div>
-                <div className="flex flex-col mt-[24px] items-end justify-end gap-2">
+                <div className='flex flex-col items-start justify-between md:items-end md:justify-end lg:items-end lg:justify-end'>
+
+                <div className="flex flex-col  md:mt-[24px] lg:mt-[24px] items-start justify-start md:items-end md:justify-end lg:items-end lg:justify-end gap-2">
                   {secondaryLinks.map((secondaryLinks) => (
                     <a key={secondaryLinks.id} href={secondaryLinks.href} className="flex  text-[10px] lg:text-[14px] leading-4 text-black">{secondaryLinks.label}</a>
                   ))}
                 </div>
-                <div className="flex items-end justify-end gap-[24px] mt-[24px]">
+                <div className="flex  items-center justify-center md:items-end md:justify-end gap-[10px] md:gap-[24px] lg:gap-[24px] mt-[10px] md:mt-[24px] lg:mt-[24px]">
                   {socials.map((social) => (
-                    <a key={social.id} href={social.href} className="text-black transition duration-200 text-[16px] lg:text-[24px] hover:text-[#60bfff] hover:no-underline">{social.component}</a>
+                    <a key={social.id} href={social.href} className="text-black transition duration-200 text-[14px] lg:text-[24px] hover:text-[#60bfff] hover:no-underline">{social.component}</a>
                   ))}
                 </div>
+                </div>
+
               </div>
             </div>
           </div>
